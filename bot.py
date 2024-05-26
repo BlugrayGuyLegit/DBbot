18d9d55a970d238f3ca1478a9339ffa28cebeaa8
Blugray <162154088+BlugrayGuyLegit@users.noreply.github.com>
2024-05-26 12:39:12 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,6 +11,10 @@ bot = discord.Client(intents=intents)
 TARGET_USER_ID = 816388175181250610  # Replace with the target user's ID
 CHANNEL_ID = 1243967560647577710  # Replace with the channel ID to send messages
 
+# Store previous status and activity to avoid spamming
+previous_status = None
+previous_activity = None
+
 @bot.event
 async def on_ready():
     print(f'Logged in as {bot.user}')
@@ -18,17 +22,27 @@ async def on_ready():
 
 @tasks.loop(seconds=10)
 async def check_user_status():
+    global previous_status, previous_activity
+
     guild = discord.utils.get(bot.guilds)
     member = guild.get_member(TARGET_USER_ID)
     
     if member:
-        if str(member.status) == 'online':
-            channel = bot.get_channel(CHANNEL_ID)
-            await channel.send(f'{member.name} is online!')
+        current_status = str(member.status)
+        current_activity = next((activity for activity in member.activities if isinstance(activity, discord.Game)), None)
         
-        for activity in member.activities:
-            if isinstance(activity, discord.Game):  # Check if the activity is a game
+        # Check if status has changed
+        if current_status != previous_status:
+            previous_status = current_status
+            if current_status == 'online':
                 channel = bot.get_channel(CHANNEL_ID)
-                await channel.send(f'{member.name} started {activity.name}!')
+                await channel.send(f'{member.name} is online!')
+
+        # Check if activity has changed
+        if current_activity != previous_activity:
+            previous_activity = current_activity
+            if current_activity:
+                channel = bot.get_channel(CHANNEL_ID)
+                await channel.send(f'{member.name} started {current_activity.name}!')
 
 bot.run(os.getenv('DISCORD_TOKEN'))
