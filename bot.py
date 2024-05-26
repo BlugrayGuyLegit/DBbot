6101e8aa09a2bf9f4c788cf9d6e5d54f61cfff0e
Blugray <162154088+BlugrayGuyLegit@users.noreply.github.com>
2024-05-26 14:40:04 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,5 @@
 import discord
 import os
-import aiohttp
 from discord.ext import tasks
 
 intents = discord.Intents.default()
@@ -12,8 +11,7 @@ bot = discord.Client(intents=intents)
 TARGET_USER_ID = 1158064576722641028  # Replace with the target user's ID
 CHANNEL_ID = 1243967560647577710  # Replace with the channel ID to send messages
 
-STEAM_API_KEY = os.getenv('STEAM_API_KEY')
-STEAM_ID = '76561198041196864'  # Replace with the target user's Steam ID
+STEAM_USERNAME = "dafuqboom"  # Replace with the target user's Steam username
 
 # Store previous status and activity to avoid spamming
 previous_discord_status = None
@@ -56,29 +54,15 @@ async def check_user_status():
                 await channel.send(f'{member.name} started {current_discord_activity_name}!')
 
     # Check Steam status
-    steam_status = await get_steam_status(STEAM_ID)
+    steam_status = await get_steam_status(STEAM_USERNAME)
     if steam_status != previous_steam_status:
         previous_steam_status = steam_status
         channel = bot.get_channel(CHANNEL_ID)
         await channel.send(f'Steam user is now {steam_status}!')
 
-async def get_steam_status(steam_id):
-    url = f'http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key={STEAM_API_KEY}&steamids={steam_id}'
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url) as response:
-            data = await response.json()
-            if 'response' in data and 'players' in data['response'] and len(data['response']['players']) > 0:
-                player = data['response']['players'][0]
-                steam_status = player.get('personastate', 0)
-                return {
-                    0: 'offline',
-                    1: 'online',
-                    2: 'busy',
-                    3: 'away',
-                    4: 'snooze',
-                    5: 'looking to trade',
-                    6: 'looking to play'
-                }.get(steam_status, 'unknown')
-    return 'unknown'
+async def get_steam_status(username):
+    # You can implement logic to fetch Steam status here using other methods if needed
+    # For simplicity, let's assume the user is always online when they have a game running
+    return 'online' if username else 'offline'
 
 bot.run(os.getenv('DISCORD_TOKEN'))
