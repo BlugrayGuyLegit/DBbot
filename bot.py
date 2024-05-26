42950927a356e6d37bd57cb789899a21cf489c2f
Blugray <162154088+BlugrayGuyLegit@users.noreply.github.com>
2024-05-26 12:41:11 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,7 +13,7 @@ CHANNEL_ID = 1243967560647577710  # Replace with the channel ID to send messages
 
 # Store previous status and activity to avoid spamming
 previous_status = None
-previous_activity = None
+previous_activity_name = None
 
 @bot.event
 async def on_ready():
@@ -22,14 +22,19 @@ async def on_ready():
 
 @tasks.loop(seconds=10)
 async def check_user_status():
-    global previous_status, previous_activity
+    global previous_status, previous_activity_name
 
-    guild = discord.utils.get(bot.guilds)
+    guild = bot.guilds[0]  # Assuming the bot is in a single guild
     member = guild.get_member(TARGET_USER_ID)
     
     if member:
         current_status = str(member.status)
         current_activity = next((activity for activity in member.activities if isinstance(activity, discord.Game)), None)
+        current_activity_name = current_activity.name if current_activity else None
+        
+        # Log the current status and activity for debugging purposes
+        print(f'Current status: {current_status}')
+        print(f'Current activity: {current_activity_name}')
         
         # Check if status has changed
         if current_status != previous_status:
@@ -39,10 +44,13 @@ async def check_user_status():
                 await channel.send(f'{member.name} is online!')
 
         # Check if activity has changed
-        if current_activity != previous_activity:
-            previous_activity = current_activity
-            if current_activity:
+        if current_activity_name != previous_activity_name:
+            previous_activity_name = current_activity_name
+            if current_activity_name:
                 channel = bot.get_channel(CHANNEL_ID)
-                await channel.send(f'{member.name} started {current_activity.name}!')
+                await channel.send(f'{member.name} started {current_activity_name}!')
+            elif previous_activity_name:  # Activity ended
+                channel = bot.get_channel(CHANNEL_ID)
+                await channel.send(f'{member.name} stopped playing {previous_activity_name}!')
 
 bot.run(os.getenv('DISCORD_TOKEN'))
